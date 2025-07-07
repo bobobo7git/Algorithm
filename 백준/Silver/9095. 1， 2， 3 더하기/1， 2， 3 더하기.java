d0b6f2c306b673c37608956ef6691e9797b50393
유보형 <121767620+bobobo7git@users.noreply.github.com>
2025-07-07 15:52:49 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] memo;
+    public static void main(String[] args) throws IOException {
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(reader);
+
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            memo = new int[N+1];
+
+            if (N >= 1) memo[1] = 1;
+            if (N >= 2) memo[2] = 2;
+            if (N >= 3) memo[3] = 4;
+            sb.append(f(N)).append('\n');
+        }
+        System.out.println(sb.toString());
+
+    }
+    static int f(int n) {
+        if (n <= 0) return 0;
+        if (memo[n] > 0) return memo[n];
+        return f(n-1) + f(n-2) + f(n-3);
+    }
+}
