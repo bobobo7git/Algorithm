1a71da7f4e5410b1f22c16ccb86101cbdc6f6c9b
유보형 <121767620+bobobo7git@users.noreply.github.com>
2025-09-01 12:00:24 +0900

    [Gold IV] Title: 고냥이, Time: 128 ms, Memory: 15692 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.java
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -1,6 +1,5 @@
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         InputStreamReader reader = new InputStreamReader(System.in);
@@ -8,47 +7,25 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         String string = br.readLine();
+        char[] arr = string.toCharArray();
 
-        int result = binarySearch(string.toCharArray(), N);
-        System.out.println(result);
-    }
-    static int binarySearch(char[] arr, int n) {
         int l = 0;
-        int r = arr.length;
-
-        while (l <= r) {
-            int mid = (l+r) / 2;
-            int result = check(arr, mid);
-            if (result <= n) {
-                l = mid+1;
-            } else {
-                r = mid-1;
-            }
-        }
-        return r;
-
-    }
-    static int check(char[] arr, int size) {
-        int l = 0;
-        int r = size;
-        int t = 0;
-
+        int r = 0;
         int[] cnt = new int[27];
-        for (int i=0; i<r; i++) {
-            cnt[arr[i] - 'a']++;
-            if (cnt[arr[i]-'a'] == 1) {
-                t++;
+        int max = 0;
+        int len = 0;
+
+        while (l < arr.length && r < arr.length) {
+            if (cnt[arr[r++]-'a']++ == 0) {
+                max++;
             }
-        }
-        int min = t;
+            while (l < r && max > N) {
+                if (--cnt[arr[l++]-'a'] == 0) max--;
+            }
+            len = Math.max(len, r-l);
 
-        for (int i=r; i<arr.length; i++) {
-            if (cnt[arr[i]-'a']++ == 0) t++;
-            if (--cnt[arr[l++]-'a'] == 0) t = Math.max(t-1, 0);
-            min = Math.min(min, t);
         }
+        System.out.println(len);
 
-        return min;
     }
 }
-
