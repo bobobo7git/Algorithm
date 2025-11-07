9aa15ec0e27aeada1eee1b0f2014d63e73b7c4d2
유보형 <121767620+bobobo7git@users.noreply.github.com>
2025-11-07 16:09:46 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,11 @@
+-- 코드를 작성해주세요
+select
+    distinct d.id,
+    d.email,
+    d.first_name,
+    d.last_name
+from developers d
+join skillcodes s
+on d.skill_code & s.code = s.code
+where s.category = 'Front End'
+order by d.id
